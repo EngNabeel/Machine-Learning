a4788ceef94c58bb53554f5f0a32233bd22d8df3
Fatma El Zahraa <zahrasamir97@gmail.com>
2019-12-11 11:55:18 +0200

    exercise 1

--- MATLAB/machine-learning-ex1/ex1/gradientDescent.m
+++ MATLAB/machine-learning-ex1/ex1/gradientDescent.m
@@ -17,10 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
+    error = (X * theta) - y;
+    result = (alpha/m) * (X' * error); 
+    theta = theta - result; 
 
 
     % ============================================================
